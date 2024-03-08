439bb6aebc0fb1731d540c18cc31c9f84bfe17bb
Henrique Reimao <henrique.reimao.dev@gmail.com>
2024-03-08 00:29:36 -0300

    - Update Angular app with routing and navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {  HomeComponent } from './pages/home/home.component';
+import { ContatosComponent } from './pages/contatos/contatos.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+    { path: 'home-component', component: HomeComponent},
+    { path: 'contatos-component' , component: ContatosComponent},
+    { path: 'sobre-component' , component: SobreComponent},
+
+];
